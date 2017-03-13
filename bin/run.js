32fd75c17d45ea29ff96ec94d7a2ede7625dab8b
Prashant <agarwalprashant355@gmail.com>
2017-03-13 22:46:57 +0530

    First Commit

---
+++ bin/run.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const slackClient = require('../server/slackClient');
+const service = require('../server/service');
+const http = require('http');
+const server = http.createServer(service);
+
+const slackToken = 'xoxb-153445930147-Tjy11gGxUW6TvCtYOYwtzG0K';
+const slackLogLevel = 'verbose';
+
+const rtm = slackClient.init(slackToken, slackLogLevel);
+rtm.start();
+
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
+
+server.on('listening', function() {
+    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
+});
